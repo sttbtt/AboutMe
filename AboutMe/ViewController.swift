4ac8392030f4cb12271788be24dd72363a3f4e20
Scott Bennett <stt.btt@gmail.com>
2019-05-09 21:41:11 -0400

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Scott Bennett on 5/9/19.
+//  Copyright © 2019 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
