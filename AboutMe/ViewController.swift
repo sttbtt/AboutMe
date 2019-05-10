e388420471ac296bef8f5f217be5a40872b40f12
Scott Bennett <stt.btt@gmail.com>
2019-05-09 22:18:51 -0400

    Add introduce button

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Outlets and Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var hobbiesLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 //        print("My name is Scott")
-        nameLabel.text = "My name is Scott"
-        hobbiesLabel.text = "iOS development"
+//        nameLabel.text = nil
+//        hobbiesLabel.text = ""
+        
+        nameLabel.isHidden = true
+        hobbiesLabel.isHidden = true
+        
     }
 
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var hobbiesLabel: UILabel!
     
+    
+    @IBAction func introductYourselfButtonPressed(_ sender: UIButton) {
+        nameLabel.isHidden = false
+        hobbiesLabel.isHidden = false
+        nameLabel.text = "My name is Scott"
+        hobbiesLabel.text = "iOS development"
+        
+    }
     
 }
 
